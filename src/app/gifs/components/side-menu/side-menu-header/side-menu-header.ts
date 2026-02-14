f25457861a886a094725cbb7cb4bb06a38df7b4a
Freddy Tomada <freddytomada7@gmail.com>
2026-02-14 13:02:29 -0400

    add gif list components

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { environment } from '@environments/environment';
-// import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'side-menu-header',
