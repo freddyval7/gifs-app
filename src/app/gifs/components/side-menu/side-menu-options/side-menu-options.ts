4c3d8e3d540b047ae03172dec721c56a3ad9b7df
Freddy Tomada <freddytomada7@gmail.com>
2026-02-16 12:51:41 -0400

    add search and history components

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Gif } from 'src/app/gifs/interfaces/gif.interface';
+import { GifService } from 'src/app/gifs/services/gifs.service';
 
 interface MenuOption {
   label: string;
@@ -14,6 +16,8 @@ interface MenuOption {
   templateUrl: './side-menu-options.html',
 })
 export class SideMenuOptions {
+  gifService = inject(GifService);
+
   menuOptions: MenuOption[] = [
     {
       icon: 'fa-solid fa-chart-line',
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
+
+const LOCAL_STORAGE_KEY = 'searchHistory';
+
+function loadGifsFromLocalStorage(): Record<string, Gif[]> {
+  const searchHistory = localStorage.getItem(LOCAL_STORAGE_KEY) ?? '{}'; //Record<string, Gif[]>
+
+  return JSON.parse(searchHistory);
+}
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
+  trendingGifsLoading = signal(true);
+
+  searchHistory = signal<Record<string, Gif[]>>(loadGifsFromLocalStorage());
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
+
+  saveGifsToLocalStorage = effect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.searchHistory()));
+  });
 
   constructor() {
     this.loadTrendingGifs();
@@ -26,7 +43,34 @@ export class GifService {
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
         this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
         console.log(gifs);
       });
+  }
+
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+          q: query,
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((data) => GifMapper.mapGiphyItemsToGifArray(data)),
+
+        tap((items) => {
+          this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase()]: items,
+          }));
+        }),
+      );
+  }
+
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
   }
 }
